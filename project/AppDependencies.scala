c7efc1ee2b46e0314c86c9908af03b6cd834efa4
spectre99x <92088633+Spectre99x@users.noreply.github.com>
2024-07-17 15:52:43 +0100

    [DI-210] Include http-verbs-30 as dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,10 +6,12 @@ import sbt._
 object AppDependencies {
 
   private val bootstrapVersion = "9.0.0"
+  private val verbsVersion = "15.0.0"
   
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion,
+    "uk.gov.hmrc"             %% "http-verbs-play-30"         % verbsVersion
   )
 
   val test = Seq(
