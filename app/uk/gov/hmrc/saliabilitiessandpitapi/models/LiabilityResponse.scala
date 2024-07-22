f1ea732cae5c7d0871fbf48d7c53b7eabd344b50
Robert Buczek <142578199+RobertBuczek@users.noreply.github.com>
2024-07-22 15:44:02 +0100

    [DI-227]]: Implement methodNotAllowed Logic for Unsupported HTTP Methods

--- app/uk/gov/hmrc/saliabilitiessandpitapi/models/LiabilityResponse.scala
+++ app/uk/gov/hmrc/saliabilitiessandpitapi/models/LiabilityResponse.scala
@@ -23,43 +23,31 @@ import play.api.mvc.Codec
 import uk.gov.hmrc.saliabilitiessandpitapi.models.integration.BalanceDetail
 
 enum LiabilityResponse:
-  case Ok(balances: Seq[BalanceDetail]) extends LiabilityResponse, SuccessResponse
-  case Error(error: String, description: String) extends LiabilityResponse, FailureResponse
-  case InvalidInputNino(description: String) extends LiabilityResponse, FailureResponse
-  case MethodNotAllowed(description: String) extends LiabilityResponse, FailureResponse
-  case NotFound(description: String) extends LiabilityResponse, FailureResponse
-  case Unauthorized(description: String) extends LiabilityResponse, FailureResponse
-  case InternalServerError(description: String) extends LiabilityResponse, FailureResponse
+  case Ok(balances: Seq[BalanceDetail])
+  case Error(error: String, description: String)
+  case InvalidInputNino(description: String)
+  case MethodNotAllowed(description: String)
+  case NotFound(description: String)
+  case Unauthorized(description: String)
+  case InternalServerError(description: String)
 
 object LiabilityResponse:
-  given Format[Ok]                                                  = format[Ok]
-  given Format[Error]                                               = format[Error]
-  given Format[InvalidInputNino]                                    = format[InvalidInputNino]
-  given Format[MethodNotAllowed]                                    = format[MethodNotAllowed]
-  given Format[NotFound]                                            = format[NotFound]
-  given Format[InternalServerError]                                 = format[InternalServerError]
-  given Format[Unauthorized]                                        = format[Unauthorized]
+  given OWrites[Ok]                                                 = writes[Ok]
+  given OWrites[Error]                                              = writes[Error]
+  given OWrites[InvalidInputNino]                                   = writes[InvalidInputNino]
+  given OWrites[MethodNotAllowed]                                   = writes[MethodNotAllowed]
+  given OWrites[NotFound]                                           = writes[NotFound]
+  given OWrites[InternalServerError]                                = writes[InternalServerError]
+  given OWrites[Unauthorized]                                       = writes[Unauthorized]
+  given Writes[LiabilityResponse]                                   = Writes {
+    case ok: Ok                                   => toJsObject(ok)
+    case error: Error                             => toJsObject(error)
+    case notFound: NotFound                       => toJsObject(notFound)
+    case unauthorized: Unauthorized               => toJsObject(unauthorized)
+    case invalidInputNino: InvalidInputNino       => toJsObject(invalidInputNino)
+    case methodNotAllowed: MethodNotAllowed       => toJsObject(methodNotAllowed)
+    case internalServerError: InternalServerError => toJsObject(internalServerError)
+  }
   given contentTypeOf: ContentTypeOf[LiabilityResponse]             = ContentTypeOf(Some(ContentTypes.JSON))
   given writeable(using codec: Codec): Writeable[LiabilityResponse] =
-    Writeable(data => codec.encode(toJson(data).toString()))
-
-  given Format[LiabilityResponse] = new Format[LiabilityResponse]:
-    def reads(json: JsValue): JsResult[LiabilityResponse] =
-      (json \ "type").as[String] match
-        case "Ok"                  => fromJson[Ok](json)
-        case "Error"               => fromJson[Error](json)
-        case "InvalidInputNino"    => fromJson[InvalidInputNino](json)
-        case "MethodNotAllowed"    => fromJson[MethodNotAllowed](json)
-        case "NotFound"            => fromJson[NotFound](json)
-        case "Unauthorized"        => fromJson[Unauthorized](json)
-        case "InternalServerError" => fromJson[InternalServerError](json)
-        case _                     => JsError("Unknown type")
-
-    def writes(o: LiabilityResponse): JsValue = o match
-      case ok: Ok                 => toJson(ok).as[JsObject] + ("type" -> JsString("Ok"))
-      case e: Error               => toJson(e).as[JsObject] + ("type"  -> JsString("Error"))
-      case e: InvalidInputNino    => toJson(e).as[JsObject] + ("type"  -> JsString("InvalidInputNino"))
-      case e: MethodNotAllowed    => toJson(e).as[JsObject] + ("type"  -> JsString("MethodNotAllowed"))
-      case e: NotFound            => toJson(e).as[JsObject] + ("type"  -> JsString("NotFound"))
-      case e: Unauthorized        => toJson(e).as[JsObject] + ("type"  -> JsString("Unauthorized"))
-      case e: InternalServerError => toJson(e).as[JsObject] + ("type"  -> JsString("InternalServerError"))
+    Writeable(data => codec.encode(toJson(data).toString))
